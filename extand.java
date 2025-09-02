4c98340c8b17c4ced25966c9e02b0604e0145ef5
Mukulkumar9162 <mukulkumar1858@gmail.com>
2025-09-02 22:39:40 +0530

    Add files via upload

---
+++ extand.java
@@ -0,0 +1,70 @@
+// public class inheritance {
+//     public static void main(String[] args) {   
+//         Father f=new Father();
+//         // Bacha b=new Bacha();
+//          Father b=new Bacha(); 
+//         // Bacha b=new father(); it's incrrect
+
+
+//         // f.show(5);
+//         // b.show();
+//         // b.show(6);
+//         f.show(7);
+//         b.show(2);
+//     }
+// }
+// class Father{
+// public static void show(int x){
+//     System.out.println("papa hu");
+
+// }
+// } 
+// class Bacha extends Father{
+//     // extends is a key
+//     //overriding 
+//     public static void show() {
+//         System.out.println("Bacha hu");
+        
+//     }
+// }
+
+// its single inheritance example
+public class inheritance {
+    public static void main(String[] args) {   
+vechile v=new vechile();
+       Car   c=new Car();
+
+       v.show();
+       c.show();
+
+
+       
+
+
+        // f.show(5);
+        // b.show();
+        // b.show(6);
+        v.show(7);
+        c.show(2);
+    }
+}
+class vechicle{
+public static void show (String){
+    System.out.println("vechicle hu");
+
+}
+} 
+
+class Car{
+    public static void show (int x,double) {
+        System.out.println("");
+        
+    }
+}
+public class extand BMW{
+    System.out.println();
+    
+    
+
+    
+}
