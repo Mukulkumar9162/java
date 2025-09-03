cc97ac42685411422218c3888ad9446571facf73
Mukulkumar9162 <mukulkumar1858@gmail.com>
2025-09-03 10:45:30 +0530

    Add files via upload

---
+++ inheritance.java
@@ -0,0 +1,67 @@
+// public class inheritance {
+//     public static void main(String[] args) {   
+//         Father f=new Father();
+//         // Bacha b=new Bacha();
+//          Father b=new Bacha(); 
+//         // Bacha b=new father(); it's incrrect
+
+
+//         // f.show(5);
+//         // b.show();
+//         // b.show(6);
+//         f.show(7);
+//         b.show(2);
+//     }
+// }
+// class Father{
+// public static void show(int x){
+//     System.out.println("papa hu");
+
+// }
+// } 
+// class Bacha extends Father{
+//     // extends is a key
+//     //overriding 
+//     public static void show() {
+//         System.out.println("Bacha hu");
+        
+//     }
+// }
+
+// its single inheritance example
+// public class inheritance {
+//     public static void main(String[] args) {   
+// vechile v=new vechile();
+//        Car   c=new Car();
+       
+
+
+//         // f.show(5);
+//         // b.show();
+//         // b.show(6);
+//         v.s(7);
+//         c.display(2);
+//     }
+// }
+// class vechicle{
+// public static string display (){
+//     System.out.println("vechicle");
+
+// }
+// } 
+
+// class Car{
+//     public static  () {
+//         System.out.println("");
+        
+//     }
+// }
+// public class extend {
+//     System.out.println();
+    
+    
+
+    
+// }
+
+
