378e97f51503909b31f9c8da3beb589bef5b8452
Mukulkumar9162 <mukulkumar1858@gmail.com>
2025-09-03 10:44:14 +0530

    Add files via upload

---
+++ forloop3.java
@@ -0,0 +1,45 @@
+ import java.util.Scanner;
+
+ public class forloop3 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        int ans=0,p=0;
+        for(int n=sc.nextInt();n>0;n/=2){
+
+            int rem =n%2;
+            int 
+
+
+        }
+       
+
+        
+       
+    
+    }
+ }
+// import java.util.Scanner;
+
+// public class ReverseNumber {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+        
+//         // step1 Input the number to be reversed
+       
+//  System.out.print("Enter a number: ");
+//         int number = scanner.nextInt();
+//         int reversedNumber = 0;
+
+//         //step Loop to reverse the number
+//         for (; number != 0; number /= 10) {
+//             int digit = number % 10; // Get the last digit
+//             reversedNumber = reversedNumber * 10 + digit; // Build the reversed number
+//         }
+
+//         // Output the reversed number
+//         System.out.println("Reversed Number: " + reversedNumber);
+        
+//         scanner.close();
+//     }
+// }
